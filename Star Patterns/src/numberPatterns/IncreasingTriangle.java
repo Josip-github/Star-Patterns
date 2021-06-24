c1092b7891b346a11e18de074e6b16796bdde40e
Josip-github <josipcota1@gmail.com>
2021-06-24 18:15:48 +0200

    Može i ovako

--- Star Patterns/src/numberPatterns/IncreasingTriangle.java
+++ Star Patterns/src/numberPatterns/IncreasingTriangle.java
@@ -14,10 +14,10 @@ public class IncreasingTriangle {
 		
 		int rows = 5;
 		
-		for(int i = 1; i <= rows; i++) {
+		for(int i = 1, p = 1; i <= rows; i++, p++) {
 			
 			for(int j = 1; j <= i; j++) {
-				System.out.print(i);
+				System.out.print(p + " ");
 			}
 			System.out.println();
 		}
