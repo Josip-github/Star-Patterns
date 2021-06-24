a5e0a43e9a685777819d68adb7b2f3818b046bc7
Josip-github <josipcota1@gmail.com>
2021-06-24 20:52:41 +0200

    Diamond Pattern 03

---
+++ Star Patterns/src/numberPatterns/DiamondPattern03.java
@@ -0,0 +1,60 @@
+package numberPatterns;
+
+public class DiamondPattern03 {
+
+	public static void main(String[] args) {
+
+		/*
+		 * 			1
+		 * 		   123
+		 * 		  12345
+		 * 		 1234567
+		 * 		123456789
+		 * 		 1234567
+		 * 		  12345
+		 * 		   123
+		 * 			1
+		 */
+		
+		int n = 5;
+		
+		for(int i = 1; i < n; i++) {
+			
+			int p = 1;
+			
+			for(int j = i; j < n; j++) {
+				System.out.print("  ");
+			}
+				
+			for(int j = 1; j < i; j++) {
+				System.out.print(p++ + " ");
+			}
+			
+			for(int j = 1; j <= i; j++) {
+				System.out.print(p++ + " ");
+			}
+			
+			System.out.println();
+		}
+		
+		for(int i = 1; i <= n; i++) {
+			
+			int p = 1;
+			
+			for(int j = 1; j < i; j++) {
+				System.out.print("  ");
+			}
+				
+			for(int j = i; j < n; j++) {
+				System.out.print(p++ + " ");
+			}
+			
+			for(int j = i; j <= n; j++) {
+				System.out.print(p++ + " ");
+			}
+			
+			System.out.println();
+		}
+	}
+
+}
