93e54f2112da377f2e39d180dd91f37f46f69d0e
Josip-github <josipcota1@gmail.com>
2021-06-25 11:41:35 +0200

    Increasing Right Sided Triangle 01

---
+++ Star Patterns/src/numberPatterns/IncreasingRightSidedTriangle01.java
@@ -0,0 +1,31 @@
+package numberPatterns;
+
+public class IncreasingRightSidedTriangle01 {
+
+	public static void main(String[] args) {
+
+		/*
+		 *     1
+		 *    22
+		 *   333
+		 *  4444
+		 * 55555       
+		 */
+		
+		int rows = 5;
+		
+		for(int i = 1, p = 1; i <= rows; i++,p++) {
+			
+			for(int j = i; j < rows; j++) {
+				System.out.print("  ");
+			}
+			
+			for(int j = 1; j <= i; j++) {
+				System.out.print(p + " ");
+			}
+			
+			System.out.println();
+		}
+	}
+
+}
